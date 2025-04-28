bb83b53647577d1775819f4c16d49bb6fc012c2f
MawinTN <mawinkung2547@gmail.com>
2025-04-28 09:26:21 +0700

    Delete project12367 directory

--- project12367/meeting_project/meeting_room/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import RoomBooking
-
-@admin.register(RoomBooking)
-class RoomBookingAdmin(admin.ModelAdmin):
-    list_display = ('name', 'room_name', 'booking_date', 'start_time', 'end_time')
